5e264e32068c58f85180bb46cd0d01f2d3ae8184
Tsuneo Yoshioka <tsuneo.yoshioka@f-secure.com>
2014-02-11 15:24:51 +0200

    Initial Commit

---
+++ GullHttpServer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GullHttpServer
+//
+//  Created by Yoshioka Tsuneo on 2/11/14.
+//  Copyright (c) 2014 Yoshioka Tsuneo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
